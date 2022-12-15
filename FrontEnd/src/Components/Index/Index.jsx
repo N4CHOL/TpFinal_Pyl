f7959dbe430e9b703310d99dc60005ef7e343649
N4CHOL <104908466+N4CHOL@users.noreply.github.com>
2022-12-15 09:27:20 -0300

    mas cambios esteticos + bug fix

--- FrontEnd/src/Components/Index/Index.jsx
+++ FrontEnd/src/Components/Index/Index.jsx
@@ -1,10 +1,29 @@
 
-import React from 'react'
+import { useState,useEffect } from "react";
 import authservice from "../../Utilities/Services/auth.service"
+import { useNavigate } from "react-router-dom";
 import "./index.css"
 function Index() {
 
+  const [currentUser , setCurentUser] = useState()
+  const navigate = useNavigate()
+
   const user= authservice.getCurrentUser()
+
+  useEffect(() => {
+    try {
+      const user = authservice.getCurrentUser();
+      //console.log(user);
+
+      if (user) {
+        setCurentUser(user);
+      } else {
+        navigate("/app/auth/login");
+      }
+    } catch (err) {
+      navigate("/app/auth/login");
+    }
+  }, [navigate]);
   return (
     <>
     <h1 className='titleindex'>Anotador</h1>
